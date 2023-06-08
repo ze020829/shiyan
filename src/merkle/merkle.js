c97260019f367b6b7c9f3a03ec2706ea5dba79cf
ze020829 <132757037+ze020829@users.noreply.github.com>
2023-06-08 22:40:44 +0800

    Update merkle.js

--- src/merkle/merkle.js
+++ src/merkle/merkle.js
@@ -1,93 +1,31 @@
-import crypto from 'crypto'
+class merkel{
 
-const generateHash = (content) => crypto.createHash("sha256").update(content).digest("hex")
+comeout(hashArray){
 
-class Node{
-    constructor(content) {
-        this.content = content
-        this.hash = generateHash(JSON.stringify({
-            content: this.content
-        }))
-    }
-
-    setHash(content){
-        this.hash = generateHash(JSON.stringify({
-            content: content
-        }))
-    }
-
+for(let i=hashArray.length-1;i>=0;i--){
+hashArray[i]=SHA256(hashArray[i]).toString()
 }
 
-class MerkleTree{
 
-    constructor(nodes) {
-        this.rootHash = "rootHash not init"
-        this.nodes = nodes
-        this.hashArray = []
-    }
-
-    // calculate the root hash
-    merkleTree(){
-        var arrLen = this.nodes.length
-        var height = Math.floor(Math.log2(arrLen)) // begin with 0
-        var heightNumber = height
-        var totalNumber = Math.pow(2,height)
-        var allNodes = []
-        for (let i = 0; i < arrLen; i++) {
-            const node = new Node(this.nodes[i])
-            allNodes.push(node)
-        }
-        for (let i = 0; i < totalNumber - arrLen; i++) {
-            const node = new Node("null")
-            allNodes.push(node)
-        }
-        let arr_ = []
-        for (let i = 0; i < arrLen; i++) {
-            arr_.push(allNodes[i])
-        }
-        this.hashArray.push(arr_)
-        for (let i = 0; i < height; i++) {
-            var nodeNumber = allNodes.length
-            var arr = []
-            var hashArr = []
-            for (let j = 0; j+1 < nodeNumber; j=j+2) {
-                if(heightNumber==1){
-                    var node = new Node(`root`)
-                }else {
-                    var node = new Node(`height:${heightNumber-1}`)
-                }
-                var temp = allNodes[j].hash+allNodes[j+1].hash
-                node.setHash(temp)
-                arr.push(node)
-                hashArr.push(node)
+    let current=hashArray
+    while(current.length>1){
+        for(let i=1;i<=current.length;i++){
+            if(i%2==0){
+                current[i/2-1]=SHA256(current[i-1]+current[i-2]).toString()
+            }else if(i==current.length&&i%2!=0){
+    current[i/2]=SHA256(current[i-1]).toString()
             }
-            heightNumber--
-            this.hashArray.push(hashArr)
-            allNodes = arr
-            nodeNumber = nodeNumber / 2
-        }
-        this.rootHash = allNodes[0].hash
-        this.hashArray.reverse()
-        return allNodes[0].hash
+       }
+    
+       if(current.length%2==0){
+        current.splice(current.length/2,current.length/2)
+    }else{
+        current.splice((current.length+1)/2,(current.length-1)/2)
     }
-
-    // verify the hash
-    verify(arr){
-        arr = arr.reverse()
-        var num = arr.length - 1
-        var hash = ""
-        for (let i = num; i > 0; i--) {
-            let temp = arr[i] + arr[i-1]
-            hash = generateHash(JSON.stringify({
-                content: temp
-            }))
-            arr.pop()
-            arr.pop()
-            arr.push(hash)
-        }
-        return arr[0] == this.rootHash
     }
-
+    
+   return current[0]
 }
 
-export default MerkleTree
+
+}
