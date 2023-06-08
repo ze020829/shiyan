234926301150d1419840cb5b80215e91faeebae3
ze020829 <132757037+ze020829@users.noreply.github.com>
2023-06-08 22:42:00 +0800

    Update tree.js

--- src/patriticTree/tree.js
+++ src/patriticTree/tree.js
@@ -1,86 +1,81 @@
-class MPT {
-    constructor() {
-        this.root = null;
-        this.data = {};
-    }
-
-
-    addOrUpdateAddress(address, balance) {
-        const key = this.getAddressKey(address);
-        this.data[key] = balance;
-        this.root = null; // 标记根节点需要重新计算
-    }
-
-    getAddressKey(address) {
-        const hash = keccak256(address); // 使用keccak256进行哈希计算
-        return '0x' + hash.toString('hex');
-    }
-
-    calculateRoot() {
-        if (this.root === null) {
-            const keys = Object.keys(this.data).sort();
-            const leaves = keys.map(key => ({ key, value: this.data[key] }));
-            this.root = this.buildMerkleTree(leaves);
-        }
-        return this.root;
-    }
-
-    buildMerkleTree(nodes) {
-        if (nodes.length === 0) {
-            return null;
-        }
-
-        if (nodes.length === 1) {
-            return this.hashNode(nodes[0]);
-        }
-
-        const nextLevelNodes = [];
-        for (let i = 0; i < nodes.length; i += 2) {
-            const left = nodes[i];
-            const right = i + 1 < nodes.length ? nodes[i + 1] : null;
-            const parentNode = this.hashNode(left, right);
-            nextLevelNodes.push(parentNode);
-        }
-
-        return this.buildMerkleTree(nextLevelNodes);
-    }
-
-    hashNode(left, right = null) {
-        if (right === null) {
-            return left;
-        }
-
-        const hashInput = left.key + left.value + right.key + right.value;
-        const hash = keccak256(hashInput);
-        const node = { key: '0x' + hash.toString('hex') };
-        return node;
-    }
-
-    verifyAddressBalance(address, balance) {
-        const key = this.getAddressKey(address);
-        return this.data[key] === balance;
-    }
+class leaf{//将Branch节点的value改成地址就可以了
+constructor(keyEnd){
+this.keyEnd=keyEnd//这里value不能一开始就定下来，所以不能在constructor里面设value
 }
 
-// 示例用法
-const mpt = new MPT();
-mpt.addOrUpdateAddress('0x95222290dd7278aa3ddd389cc1e1d165cc4bafe5', 100);
-mpt.addOrUpdateAddress('0x1a5e3c819b61563cfc2f5b6d9b2e169a0d1e0609', 200);
+setValue(value){
+this.value=value
+}
 
-const root = mpt.calculateRoot();
-console.log('Root:', root);
+speakValue(){
+    console.log(this.value)
+}
 
-console.log('Verify Address 1:', mpt.verifyAddressBalance('0x95222290dd7278aa3ddd389cc1e1d165cc4bafe5', 100));
-console.log('Verify Address 2:', mpt.verifyAddressBalance('0x1a5e3c819b61563cfc2f5b6d9b2e169a0d1e0609', 200));
-
-//更新地址余额
-mpt.addOrUpdateAddress('0x95222290dd7278aa3ddd389cc1e1d165cc4bafe5', 300);
-console.log('Verify Address 1:', mpt.verifyAddressBalance('0x95222290dd7278aa3ddd389cc1e1d165cc4bafe5', 300));
-
-//查看根节点
-const root2 = mpt.calculateRoot();
-console.log('Root:', root2);
+}
 
 
+class Branch{
+    constructor(){
+        for(let i=0x0;i<0xf;i++){
+            this.arr[i]=null
+        }
+
+    }
+addValue(value){
+    if(value!=null){
+        this.value=value
+    }
+}
+addKey(node,iterator){//传入不定数目的参数
+        this.arr[iterator]=node
+}
+speakValue(){
+    console.log(this.value)
+}
+}
+
+class createMPT{
+constructor(rootValue){
+    this.rootValue=rootValue
+this.root=new leaf(rootValue)
+let Branch0=new Branch()
+root.setValue(Branch0)
+}
+
+addBranch(...args){
+let br=new Branch()
+br.addKey(...args)
+}
+
+addLeaf(key,value){
+let l=new leaf(key,value)
+}
+//最后一个leaf可以长达四个字符，该方法要保证前两个字符和我们预定的字符一样
+create(...longStirng){
+
+    if(longStirng[0]+longStirng[1]!=this.rootValue||longStirng.length!=7){
+        throw new Error("incrrorct first value")
+    }
+    
+  if(this.Branch0[longStirng[2]]==null){
+    let sp=longStirng.slice(3,longStirng.length-1)
+    let sum
+    for (const iterator of sp) {
+        sum+=iterator
+    }
+    let leaf1=new leaf(sum)
+    this.Branch0.addKey(leaf1,longStirng[2])
+
+  }else{
+    let ext0=new leaf(longStirng[3]+longStirng[4])
+    let ext1=new leaf(longStirng[3]+longStirng[4])
+
+    this.Branch0.addKey(ext0,longStirng[2])
+    this.Branch1.addKey(ext1,longStirng[2])
+
+  }
 
 
+}
+
+}
